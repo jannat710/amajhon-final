5a649dd0d198861689e90177e11ba724990a3bab
e <jannatul0040@gmail.com>
2023-09-21 17:11:49 +0600

    first commit

---
+++ src/hooks/useHookForm.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+
+const useHookForm = (initialState = "") => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+      });
+
+  const handleChangeFormValue = (e)=>{
+
+    const {name,value} = e.target
+ 
+    setFormData({
+        ...formData,
+        [name] : value
+    })
+  }
+
+  return {
+    formData,handleChangeFormValue
+  }
+};
+
+export default useHookForm;
---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+
+const DashboardLayout = () => {
+  return (
+    <div className="flex ">
+      <div className="w-[20%] bg-red-200">
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+    
+          <li>
+            <Link to="/dashboard/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/editProfile">Edit Profile</Link>
+          </li>
+        </ul>
+      </div>
+
+
+      <div className="w-[80%]">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
